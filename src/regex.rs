44325746e7996f6b5238a426239aaf6017bad8d6
Parker Timmerman <parker@parkertimmerman.com>
2019-09-04 00:22:12 -0400

    moving doc comment

--- src/regex.rs
+++ src/regex.rs
@@ -1,9 +1,8 @@
 use regex::Regex;
 
+/// A valid phone number is a 10 digit number preceeded by "+1"
+/// e.g. +18559108712
 pub fn valid_number_re(input: &str) -> bool {
-    /// A valid phone number is a 10 digit number preceeded by "+1"
-    /// e.g. +18559108712
     let re = Regex::new(r#"\+1[\d]{10}"#).unwrap();
-
     re.is_match(input)
 }
